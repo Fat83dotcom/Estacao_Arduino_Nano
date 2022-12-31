f6ab09e8ff38437a4a37256806d9b2f2cf0a4f14
fat83dotcom <fernando.quimic@gmail.com>
2022-12-31 03:16:39 -0300

    Renomeação e contextalização de variáveis - Atribuição de um valor inicial paras as variaveis de média, para eliminar valores zerados no ininicio dos registros

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,19 @@
 #include <math.h>
 #include "myClassAndFunctions.h"
 
-const int led = 13;
-const int sensorK10 = 0;
-double mediaUmi;
-double mediaTemp;
-double mediaPress;
-double media10k;
-int bytesRecebidos;
-
 Adafruit_BME280 bme;
 Temporarios t;
 FiltraNaN filtroNaN;
 
+const int led = 13;
+const int sensorK10 = 0;
+int bytesRecebidos;
+double mediaUmi = filtroNaN.umi_NaN(bme.readHumidity(), t.pt_U);
+double mediaPress = filtroNaN.press_NaN((bme.readPressure() / 100.0F), t.pt_P);
+double mediaTemp = filtroNaN.temp_NaN(bme.readTemperature(), t.pt_T);
+double media10k = filtroNaN.t10k_NaN(getTemp(sensorK10), t.pt_10);
+
 void setup() {
-  
   pinMode(led, OUTPUT);
   pinMode(sensorK10, INPUT);
   Serial.begin(9600);
@@ -37,12 +36,13 @@ void emissorDados(
   double mediaUmi, 
   double mediaPess, 
   double mediaTemp, 
-  double media10k);
+  double media10k
+);
 
 void hardWorker() {
-  static unsigned long tempoCorrente0 = millis();
+  static unsigned long tempoCorrente = millis();
   static double soma10k, somaUmi, somaTemp, somaPress;
-  static int cont = 0;
+  static int contadorMedia = 0;
   static int minuto, hora, dias;
   const int divisor = 20;
   minuto = hora = dias = 0;
@@ -50,12 +50,12 @@ void hardWorker() {
   hora = minuto / 60;
   dias = hora / 24;
   
-  if (cont < divisor) {
+  if (contadorMedia < divisor) {
     somaUmi += filtroNaN.umi_NaN(bme.readHumidity(), t.pt_U);
-    somaTemp += filtroNaN.temp_NaN(bme.readTemperature(), t.pt_T);
     somaPress += filtroNaN.press_NaN((bme.readPressure() / 100.0F), t.pt_P);
+    somaTemp += filtroNaN.temp_NaN(bme.readTemperature(), t.pt_T);
     soma10k += filtroNaN.t10k_NaN(getTemp(sensorK10), t.pt_10);
-    cont++;
+    contadorMedia++;
     digitalWrite(led, 0);
   }
   else {
@@ -64,18 +64,18 @@ void hardWorker() {
     mediaPress = somaPress / divisor;
     mediaTemp = somaTemp / divisor;
     media10k = soma10k / divisor;
-    cont = 0;
+    contadorMedia = 0;
     somaUmi = somaTemp = somaPress = soma10k = 0;
     digitalWrite(led, 1);
   }
 
-  if ((millis() - tempoCorrente0) < 999) {
-    visor(mediaUmi, mediaPress, mediaTemp, media10k, dias, hora, minuto, cont);
+  if ((millis() - tempoCorrente) < 999) {
+    visor(mediaUmi, mediaPress, mediaTemp, media10k, dias, hora, minuto, contadorMedia);
   }
   
-  if ((millis() - tempoCorrente0) > 1000) {
+  if ((millis() - tempoCorrente) > 1000) {
 
-    tempoCorrente0 = millis();
+    tempoCorrente = millis();
   }
 }
 
